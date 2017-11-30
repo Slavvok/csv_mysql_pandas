948cde1f90bfa0e1bcd6d18ce2b418a04072238f
Slavvok <slavvok@gmail.com>
2017-11-30 04:38:19 +0300

    Two test scripts

---
+++ anomaly_detection.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+import pandas as pd
+import numpy as np
+# Uncomment to see all rows and columns in pythons shell output
+#pd.options.display.max_rows = 999
+#pd.options.display.max_columns = 9
+
+engine = create_engine("mysql+mysqldb://root:root@localhost/anomalydb?charset=utf8")
+df = pd.read_sql("main", engine)
+# Calculating standart deviation for each row
+sigma = df.groupby(['api_name', 'http_method'])['count_http_code_5xx'].transform(np.std)
+# In case count instanse > 3, is_anomaly takes the value 1, else 0
+df['is_anomaly'] = np.where(df['count_http_code_5xx'] > 3*sigma, '1', '0')
+# Rewritting db table main
+df.to_sql(name='main', con=engine, if_exists='replace', index = False, chunksize=4)
---
+++ metric_creation.py
@@ -0,0 +1,24 @@
+import csv
+import pandas as pd
+from pandas.io import sql
+from sqlalchemy import create_engine
+# Creating db connection
+engine = create_engine("mysql+mysqldb://root:root@localhost/anomalydb?charset=utf8")
+
+df = pd.read_csv('raw_data.csv', sep=',', parse_dates=True)
+# Setting index for time indexing
+df = df.set_index(['ts'])
+df.index = pd.to_datetime(df.index, format = '%Y-%m-%d %H:%M:%S')
+# Creating new table with api*mtd pairs for cases when the cod is an error.
+# Counting number of errors for each pair for each 15 mins
+two = df[df.cod >= 500].groupby(['api_name', 'mtd']).resample('15min').size()
+# Changing GroupBy type to DataFrame type
+two = two.reset_index()
+# Creating new column labels
+two.columns=['api_name', 'http_method', 'timeframe_start', 'count_http_code_5xx']
+# Rearraging column labels
+two.reindex(['timeframe_start','api_name', 'http_method', 'count_http_code_5xx'], axis=1)
+
+# Database output
+two.to_sql(name='main', con=engine, if_exists='append', index = False, chunksize=4)
+
