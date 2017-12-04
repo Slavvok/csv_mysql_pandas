e61bb092143c42bf1715cdf989423f11feb1c3ee
Slavvok <slavvok@gmail.com>
2017-12-04 15:46:47 +0300

    Code was put inside def Main

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -5,11 +5,15 @@ import numpy as np
 #pd.options.display.max_rows = 999
 #pd.options.display.max_columns = 9
 
-engine = create_engine("mysql+mysqldb://root:root@localhost/anomalydb?charset=utf8")
-df = pd.read_sql("main", engine)
-# Calculating standart deviation for each row
-sigma = df.groupby(['api_name', 'http_method'])['count_http_code_5xx'].transform(np.std)
-# In case count instanse > 3, is_anomaly takes the value 1, else 0
-df['is_anomaly'] = np.where(df['count_http_code_5xx'] > 3*sigma, '1', '0')
-# Rewritting db table main
-df.to_sql(name='main', con=engine, if_exists='replace', index = False, chunksize=4)
+def Main():
+    engine = create_engine("mysql+mysqldb://root:root@localhost/anomalydb?charset=utf8mb4")
+    df = pd.read_sql("main", engine)
+    # Calculating standart deviation for each row
+    sigma = df.groupby(['api_name', 'http_method'])['count_http_code_5xx'].transform(np.std)
+    # In case count instanse > 3, is_anomaly takes the value 1, else 0
+    df['is_anomaly'] = np.where(df['count_http_code_5xx'] > 3*sigma, '1', '0')
+    # Rewritting db table main
+    df.to_sql(name='main', con=engine, if_exists='replace', index = False, chunksize=4)
+
+if __name__ == '__main__':
+    Main()
